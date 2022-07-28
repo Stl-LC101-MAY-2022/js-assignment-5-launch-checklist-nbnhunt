b0a42542789811473ca20e0a543fba41072b4adc
nbnhunt <nbnhunt@outlook.com>
2022-07-28 13:17:36 -0500

    no message, just a save...

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
 // Write your JavaScript code here!
 // Make sure to call your formSubmission() function at the appropriate time in your script.js file!
-const helperScript = require('./scriptHelper.js');
+// import { formSubmission } from './scriptHelper.js';
 
 
 window.addEventListener("load", function() {
 
-    helperScript.formSubmission
+    let form = document.querySelector("form");
 
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
+    form.addEventListener("submit", function(event) {
+        let pilotName = document.querySelector("input[name=pilotName]");
+        let copilotName = document.querySelector("input[name=copilotName]");
+        let fuelLevel = document.querySelector("input[name=fuelLevel]");
+        let cargoMass = document.querySelector("input[name=cargoMass]");
+        if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
+            alert("All fields are required!");
+            event.preventDefault();
+        }
+        // formSubmission(document, form, pilotName, copilotName, fuelLevel, cargoMass);
+    });
+
+    let listedPlanets;
+    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+    let listedPlanetsResponse;
+    listedPlanetsResponse.then(function (result) {
+        listedPlanets = result;
+        console.log(listedPlanets);
+    }).then(function () {
+        console.log(listedPlanets);
+        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+    })
    
 });
+
--- scriptHelper.js
+++ scriptHelper.js
@@ -1,24 +1,27 @@
-// Write your helper functions here!
+// // Write your helper functions here!
 require('isomorphic-fetch');
 
-function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-   // Here is the HTML formatting for our mission target div.
-   /*
-                <h2>Mission Destination</h2>
-                <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
-                    <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
-                </ol>
-                <img src="">
-   */
-}
 
+
+// function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
+//    // Here is the HTML formatting for our mission target div.
+//    /*
+//                 <h2>Mission Destination</h2>
+//                 <ol>
+//                     <li>Name: </li>
+//                     <li>Diameter: </li>
+//                     <li>Star: ${star}</li>
+//                     <li>Distance from Earth: </li>
+//                     <li>Number of Moons: </li>
+//                 </ol>
+//                 <img src="">
+//    */
+// }
+
+// so the first if statement doesn't return the right thing, because it's not testing for a negative, it's just the function taking a parameter...
 function validateInput(testInput) {
     if (isNaN(testInput)) {
-        return("Not a Number");
+        return("Not a Number"); 
     } 
     if (isNaN('')) {
         return("Empty");
@@ -28,41 +31,22 @@ function validateInput(testInput) {
 
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-    let form = document.querySelector("form");
-    form.addEventListener("submit", function(event) {
-        let pilotName = document.querySelector("input[name=pilotName]");
-        let copilotName = document.querySelector("input[name=copilotName]");
-        let fuelLevel = document.querySelector("input[name=fuelLevel]");
-        let cargoMass = document.querySelector("input[name=cargoMass]");
-        if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
-            alert("All fields are required!");
-            event.preventDefault();
-        }
-        // if (validateInput(pilot) === "Not a Number") {
-        //     document.li.pilotStatus.innerHTML = `${pilotName} is ready for launch`;         
-        // }
-        // if (validateInput(copilot) === "Not a Number") {
-        //     copilotStatus.innerHTML = `${copilotName} is ready for launch`; 
-        // }
-    });
-    // The list of shuttle requirements, the div with the id faultyItems, should be updated if something is not ready for launch. 
-    // Using template literals, update the li elements pilotStatus and copilotStatus to include the pilot's name and the co-pilot's name.
-    // If the user submits a fuel level that is too low (less than 10,000 liters), change faultyItems to visible with an updated fuel status stating that there is not enough fuel for the journey. The text of the h2 element, launchStatus, should also change to "Shuttle not ready for launch" and the color should change to red.
-    // If the user submits a cargo mass that is too large (more than 10,000 kilograms), change the list to visible with an updated cargo status stating that there is too much mass for the shuttle to take off. The text of launchStatus should also change to "Shuttle not ready for launch" and the color should change to red.
-    // If the shuttle is ready to launch, change the text of launchStatus to green and display "Shuttle is ready for launch".
+   // if the form is submitted and passes through script, this function will take in a document as a parameter, with strings representing form inputs, and updating the <div id=faultyItems> list using template literals
+
+
 }
 
-async function myFetch() {
-    let planetsReturned;
+// async function myFetch() {
+//     let planetsReturned;
 
-    planetsReturned = await fetch().then( function(response) {
-        });
+//     planetsReturned = await fetch().then( function(response) {
+//         });
 
-    return planetsReturned;
-}
+//     return planetsReturned;
+// }
 
-function pickPlanet(planets) {
-}
+// function pickPlanet(planets) {
+// }
 
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
